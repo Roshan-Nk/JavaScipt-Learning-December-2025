91eff084d1dfe28ed209576a66bc2b149f9993d8
Roshan-Nk <roshhtest@gmail.com>
2025-12-22 15:18:52 +0530

    Added 6 Hours temp, Day & Time
    Added ability to get current time and pull temperature for current time, and also show temperature for next 6 hours.

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ So i found another API (geoapify) that takes in user location and returns many i
 First i started by creating basic Ui layout to get idea of what info i want to display and how it should appear on desktop vs mobile. 
 
 ![alt text](<Screenshot from 2025-12-21 12-45-10.png>)
+
+Day 2 
+
+I fixed the site to show date, day and time. I also made the changes to show next 6 hour temperature with time. I learned about Date object, and using "?" as a concise if/else statement. 
+
+I am pretty confident about using API and with basic javascript so far. Though i just learned that showing API key directly within website code isnt the best practice as that can be abused, so i will have to learn how to go about that.
+
--- script.js
+++ script.js
@@ -4,11 +4,30 @@ const searchingTxt=document.querySelector("#searching");
 let long;
 let lat;
 let timeZone;
+const currentDate = new Date();
+let Weekday =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const currentDay = currentDate.getDay();
+const currentDateA = currentDate.getDate();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+const currentHour = currentDate.getHours();
+const futureTime =[];
+let hoursFormated;
+
+
 
 function searchBtn(){
     const input=document.querySelector("input");
     searchTerm=(input.value);
+    futureTime.push(currentHour);
+    let hour =currentHour+1;
+    for(let i=0;i<6;i++){
+        futureTime.push(hour);
+        hour++
+    }
+    console.log(futureTime);
     getLongAndLat();
+    
 }
 
 async function getLongAndLat(){
@@ -45,19 +64,47 @@ async function getLongAndLat(){
 async function getData(){
     try{
         let tempNow;
+        let tempArray;
         let currentTemp= document.querySelector("#todayTemp");
         console.log("Finding data...")
         const url= new URL("https://api.open-meteo.com/v1/forecast?");
         url.searchParams.set("latitude",lat);
         url.searchParams.set("longitude",long);
         url.searchParams.set("timezone",timeZone)
-        const call=await fetch(url+"&current=temperature_2m");
+        url.searchParams.set("current","temperature_2m")
+        url.searchParams.set("hourly","temperature_2m")
+        console.log(url)
+        const call=await fetch(url);
         if(!call.ok){
             throw new Error(`${call.status} Something went wrong with open-meteo`);
         }
         const data = await call.json();
         tempNow = data.current.temperature_2m
         currentTemp.innerHTML = `${tempNow}&deg;C`;
+
+
+        hoursFormated=futureTime.map(f=>{
+            let period = f<=11 ? "AM":"PM";
+            let hour12= f = f%12 || 12;
+            return hour12 +" "+period;
+        })
+
+
+        document.getElementById("day1").innerText=data.hourly.temperature_2m[futureTime[0]];
+        document.getElementById("day2").innerText=data.hourly.temperature_2m[futureTime[1]];
+        document.getElementById("day3").innerText=data.hourly.temperature_2m[futureTime[2]];
+        document.getElementById("day4").innerText=data.hourly.temperature_2m[futureTime[3]];
+        document.getElementById("day5").innerText=data.hourly.temperature_2m[futureTime[4]];
+        document.getElementById("day6").innerText=data.hourly.temperature_2m[futureTime[5]];
+        document.getElementById("todayDate").innerText=`${currentDateA}/${currentMonth}/${currentYear}`;
+        document.getElementById("todayDay").innerText=Weekday[currentDay];
+        document.getElementById("todayTime").innerText=hoursFormated[0];
+        document.getElementById("day1Time").innerText=hoursFormated[1];
+        document.getElementById("day2Time").innerText=hoursFormated[2];
+        document.getElementById("day3Time").innerText=hoursFormated[3];
+        document.getElementById("day4Time").innerText=hoursFormated[4];
+        document.getElementById("day5Time").innerText=hoursFormated[5];
+        document.getElementById("day6Time").innerText=hoursFormated[6];
         
     }
     catch(err){
